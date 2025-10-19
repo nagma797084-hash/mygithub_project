e293ae5a4f50f71f038d98b0e5ba25076ca81987
nagmakhatoon0011 <nagmaedugaon786@gmail.com>
2025-10-19 19:54:59 +0530

    1 changed in this app

---
+++ lib/my_file.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class MyFile extends StatefulWidget {
+  const MyFile({super.key});
+
+  @override
+  State<MyFile> createState() => _MyFileState();
+}
+
+class _MyFileState extends State<MyFile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
